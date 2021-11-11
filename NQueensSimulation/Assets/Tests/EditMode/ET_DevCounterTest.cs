ef4833ff0c8c3ceb880ba48c09b02c3e86f7b146
John Murwin <john.murwin@gmail.com>
2021-11-10 22:42:26 -0500

    Super Successful CodeCoverage Configuration (DONT BREAK)

---
+++ NQueensSimulation/Assets/Tests/EditMode/ET_DevCounterTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ET_DevCounterTest
+    {
+        [Test]
+        public void TestIncrement () 
+        {
+            //Assign
+            var counter = new DEV_BasicCounter(0);
+
+            //Act
+            counter.Increment();
+
+            //Assert
+            Assert.AreEqual(1, counter.Count);
+
+        }
+
+        [Test]
+        public void TestMaxCount ()
+        {
+        
+                var counter = new DEV_BasicCounter(10);
+
+                counter.Increment();
+
+                Assert.AreEqual(10, counter.Count);
+            
+        }
+    }
+}
--- NQueensSimulation/Assets/Tests/PlayMode/PT_DemoTest.cs
+++ NQueensSimulation/Assets/Tests/PlayMode/PT_DemoTest.cs
@@ -6,11 +6,11 @@ using UnityEngine.TestTools;
 
 namespace Tests
 {
-    public class PT_Default
+    public class PT_DemoTest
     {
         // A Test behaves as an ordinary method
         [Test]
-        public void PT_DefaultSimplePasses()
+        public void PT_DemoTestSimplePasses()
         {
             // Use the Assert class to test conditions
         }
@@ -18,7 +18,7 @@ namespace Tests
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator PT_DefaultWithEnumeratorPasses()
+        public IEnumerator PT_DemoTestWithEnumeratorPasses()
         {
             // Use the Assert class to test conditions.
             // Use yield to skip a frame.
