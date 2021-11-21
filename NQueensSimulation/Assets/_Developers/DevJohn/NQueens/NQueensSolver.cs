98c9b357fa083d120952507df7d586606c909f70
John Murwin <john.murwin@gmail.com>
2021-11-20 20:02:35 -0500

    Foundation for NQueens Solver Class

---
+++ NQueensSimulation/Assets/_Developers/DevJohn/NQueens/NQueensSolver.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace NQueensSimulation
+{
+    /// <summary>
+    /// The NQueens Algorithm Solver Component
+    /// </summary>
+    public class NQueensSolver : MonoBehaviour
+    {
+        #region Fields
+        //* Public Variables
+
+
+        //* Protected Variables
+        Board queensBoard = new Board();
+
+        //* Private Variables
+        [SerializeField]
+        [Tooltip("Determines the starting number of Queens (and by extension the size of the board)")]
+        private int numberOfQueens = 4;
+
+
+        #endregion
+
+
+        //* Any UNITY methods go here
+        #region LifeCycle
+        private void Start() {
+
+            Debug.Log("Starting number of queens: " + numberOfQueens);
+
+            queensBoard.ConstructBoard(numberOfQueens);
+
+            Solve();
+        }
+
+        #endregion
+
+        //? ALL Public methods go here
+        #region PublicMethods
+
+        /// <summary>
+        /// Solves the NQueens Algorithm based off the input value: numberOfQueens
+        /// </summary>
+        /// <returns>void</returns>
+        public void Solve()
+        {
+            Debug.Log(queensBoard.PrintBoard());
+        }
+
+        #endregion
+
+        //! All Private methods go here
+        #region PrivateMethods
+        
+
+        #endregion
+    }
+}
